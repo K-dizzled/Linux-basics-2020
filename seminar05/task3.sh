fb748cef74e188f252c0fefbecfebc69ac7554b1
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-10-10 16:44:18 +0300

    issue[9]

--- seminar05/task3.sh
+++ seminar05/task3.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# set this to your active development branch
-develop_branch="hw5-dev"
-current_branch="$(git rev-parse --abbrev-ref HEAD)"
-
-# only check commit messages on main development branch
-[ "$current_branch" != "$develop_branch" ] && exit 0
-
-# regex to validate in commit msg
 commit_regex='issue[+./[0-9]+]'
 error_msg="Aborting commit. Your commit message is missing either an issue[...]"
 
